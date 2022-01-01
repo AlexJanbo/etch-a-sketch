bd849becdc7b987a0961360f42ba9d9eb09aee1b
Alex <Alexanderjanbo98@gmail.com>
2022-01-01 05:29:39 -0800

    Changed so that createGrid function does not append rowElements. This allowed me to properly format the grid to have a fixed gridsize of 960x960.

--- script.js
+++ script.js
@@ -8,18 +8,23 @@ function createGrid(size) {
     containerElement.style.setProperty('--size', size)
     
     for(let i = 0; i < size; i++) {
-        let rowElement = document.createElement("div");
+        
+        /*
+        let rowElement = document.createElement("div")
         rowElement.className = "grid-rows";
         containerElement.appendChild(rowElement);
+        */
+        
+        
     }
-    for(let j = 0; j < rows.length; j++) {
+    for(let j = 0; j < size; j++) {
         for (let y = 0; y < size; y ++) {
         let columnElement = document.createElement("div");
         columnElement.className = "grid-columns";
         columnElement.addEventListener('mouseenter', function(event) {
             columnElement.style.backgroundColor = 'black';
         });
-        rows[y].appendChild(columnElement);
+        containerElement.appendChild(columnElement);
         }
     }
 
